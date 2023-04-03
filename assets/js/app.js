79ec10347ecfd66775026b1034b00c95f3eb539b
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-03 10:55:02 +0200

    add dynamic me + fix overflow body

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,30 @@ Milestone 2
 
 - Click sul contatto mostra la conversazione del contatto cliccato.
 
+Milestone 3
+- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde.
+
+- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+- Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+“mar” rimangono solo Marco e Martina).
+
+Milestone 5 - opzionale
+- Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+permette di cancellare il messaggio selezionato.
+
+- Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti.
+
+Consigli utili:
+- Si possono trascurare le scrollbar verticali, sia nel pannello dei messaggi, che nella
+lista dei contatti
+
+- I pulsanti e le icone possono non funzionare (a parte l’invio del messaggio).
+
+- Per gestire le date, può essere utile la libreria Luxon.
+
 */
 
 const { createApp } = Vue
@@ -20,6 +44,10 @@ const { createApp } = Vue
      return {
         activeContact: 0,
         newMessage: '',
+        me: {
+            name: 'Sofia',
+            avatar: './assets/img/avatar_io.jpg',
+        },
         contacts: [
             {
                 name: 'Michele',
